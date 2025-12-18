ae77c525df9ca753508e83f370aeec49155a89f3
anees <mohdjasil2004@gmail.com>
2025-12-18 12:26:20 +0530

    slash is remove from start i api

--- src/api/order.js
+++ src/api/order.js
@@ -1,7 +1,7 @@
 import { API } from "./axios";
 
 export const CreateOrderAPI = (data) => 
-  API.post("/orders/v1/create/", data);
+  API.post("orders/v1/create/", data);
 
-export const fetchMyOrders = () => API.get("/orders/v1/myorders/");
-export const fetchOrderDetail = (id) => API.get(`/orders/v1/myorders/${id}/`);
+export const fetchMyOrders = () => API.get("orders/v1/myorders/");
+export const fetchOrderDetail = (id) => API.get(`orders/v1/myorders/${id}/`);
