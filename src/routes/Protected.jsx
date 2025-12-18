c98ca459fcf2dc3764ec7a029928f0f124d7eddf
anees <mohdjasil2004@gmail.com>
2025-12-18 12:15:16 +0530

    changed the seller application

--- src/routes/Protected.jsx
+++ src/routes/Protected.jsx
@@ -2,14 +2,14 @@ import React, { useContext } from 'react'
 import { globelcontext } from '../context/userConetxt'
 import { Navigate,Outlet  } from 'react-router-dom'
 
-function Protected() {
+function Protected({children}) {
     const { user, loading } = useContext(globelcontext);
 
     if (loading) return <p>Loading...</p>;
 
     if (!user) return <Navigate to="/login"  replace />;
 
-    return <Outlet/>;
+    return children;
 }
 
 
